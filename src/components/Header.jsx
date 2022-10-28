1e454c5620baac0a3b2f69e7c04204bfdc617206
GREENFONTS <godwillonyewuchii@gmail.com>
2022-10-28 17:23:11 +0100

    Completed the profile eader section

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="profile">
+        <div>
+          <img src="assets/Mypic.jpg" id="profile_img" alt="profile_img" />
+
+          <div id="twitter">GODWILLONYEWUC1</div>
+
+          <div id="slack">Elevio</div>
+        </div>
+
+        <img id="share" src="assets/Icon.png" alt="shareIcon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
