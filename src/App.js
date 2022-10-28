1c135e924a43dee813c2116ff8b80cad3347c98f
GREENFONTS <godwillonyewuchii@gmail.com>
2022-10-28 17:24:20 +0100

    completed the links section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Links from './components/links';
 
 function App() {
   return (
     <div className="App">
       <div>
         <Header />
+        <Links />
         
       </div>
       
---
+++ src/components/links.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Links = () => {
+  return (
+    <div className="links_section">
+      <a
+        className="links"
+        href="https://training.zuri.team/"
+        target="_blank"
+        id="btn_zuri"
+        rel="noreferrer"
+      >
+        Twitter Link
+      </a>
+
+      <a
+        className="links"
+        href="http://books.zuri.team/"
+        target="_blank"
+        id="books"
+        rel="noreferrer"
+      >
+        Zuri Books
+      </a>
+
+      <a
+        className="links"
+        href="https://books.zuri.team/python-for-beginners?ref_id=elevio"
+        target="_blank"
+        id="books"
+        rel="noreferrer"
+      >
+        Python Book
+      </a>
+
+      <a
+        className="links"
+        href="https://background.zuri.team/"
+        target="_blank"
+        id="pitch"
+        rel="noreferrer"
+      >
+        Background Check for Coders
+      </a>
+
+      <a
+        className="links"
+        href="https://books.zuri.team/design-rules"
+        target="_blank"
+        id="book_design"
+        rel="noreferrer"
+      >
+        Background Check for Coders
+      </a>
+
+      <div className="icons">
+        <img src="assets/github.png" id="github" alt="github_img" />
+        <img src="assets/slack.png" id="slackImg" alt="slack_img" />
+      </div>
+    </div>
+  );
+};
+
+export default Links;
