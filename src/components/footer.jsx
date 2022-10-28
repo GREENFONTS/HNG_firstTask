74567c2ccfc755cb9c4e2b391d18577f221019e0
GREENFONTS <godwillonyewuchii@gmail.com>
2022-10-28 17:25:02 +0100

    completed the footer section

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <img src="assets/zuri.png" id="zuri" alt="zuri_img" />
+      </div>
+
+      <div>HNG Internship 9 Frontend Task</div>
+
+      <div>
+        <img src="assets/I4G.png" id="i4g" alt="I4G_img" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
